dff94726276a8f812b6d8dcdd0ad609f1cc4c1ad
shafiq1675 <islam.mdshafiq@gmail.conm>
2018-04-23 09:46:19 +0600

    Simple API test

---
+++ ApiApp/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using ApiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Helpers;
+using System.Web.Http;
+
+namespace ApiApp.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET myApi/values
+        public IEnumerable<Student> Get()
+        {
+            List<Student> li=new List<Student>();
+            Student aList=new Student ();
+            Student aList2=new Student ();
+
+            aList.ID="1";
+            aList.Name="Khan";
+            aList.Address="Dhaka";
+
+            aList2.ID = "2";
+            aList2.Name = "Shafiq";
+            aList2.Address = "Dhaka";
+
+            li.Add(aList);
+            li.Add(aList2);
+
+            return li;
+           
+        }
+
+        // GET myApi/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST myApi/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT myApi/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE myApi/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ApiApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiApp.Models
+{
+    public class Student
+    {
+        public string ID { set; get; }
+        public string Name { set; get; }
+        public string Address { set; get; }
+
+    }
+}
